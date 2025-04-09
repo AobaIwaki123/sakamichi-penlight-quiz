7bb6522e16eb37bdc08db6c66863f379cfea9635
AobaIwaki <aobaiwaki@gmail.com>
2025-04-09 14:36:31 +0000

    add: 500 page

---
+++ view/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { ServerError500 } from "@/components/Error/ServerError/ServerError500";
+
+export default function SerevrError() {
+  return (
+    <ServerError500 />
+  );
+}
