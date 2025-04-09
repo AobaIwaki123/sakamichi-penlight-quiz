c975f95cedf269b22ec3f2f8854666d1ba0ba97d
AobaIwaki <aobaiwaki@gmail.com>
2025-04-09 13:12:44 +0000

    add: footer

--- view/components/Footer/Footer.tsx
+++ view/components/Footer/Footer.tsx
@@ -1,14 +1,7 @@
+import { FooterSocial } from "./FooterSocial/FooterSocial";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>© 2023 My Application. All rights reserved.</p>
-      <nav>
-        <ul>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-          <li>Contact Us</li>
-        </ul>
-      </nav>
-    </footer>
+    <FooterSocial/>
   );
 }
