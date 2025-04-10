8968c845adb08cb2b5725d5a53deae77aecb1d96
AobaIwaki <aobaiwaki@gmail.com>
2025-04-10 10:37:56 +0000

    update: page

--- view/app/page.tsx
+++ view/app/page.tsx
@@ -4,10 +4,10 @@ import Home from '@/components/Home/Home';
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <Header />
       <Home />
       <Footer />
-    </div>
+    </>
   )
 }
