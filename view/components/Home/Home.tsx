452fa9655ab0c65e6f17821eb26efaed9da8bf56
AobaIwaki <aobaiwaki@gmail.com>
2025-04-10 12:02:32 +0000

    refactor: member infoを1つに

--- view/components/Home/Home.tsx
+++ view/components/Home/Home.tsx
@@ -4,14 +4,14 @@ import { Container, Grid } from '@mantine/core';
 import { MemberImage } from "./MemberImage/MemberImage";
 import { MemberInfoHeader } from "./MemberInfoHeader/MemberInfoHeader";
 import { PenlightForm } from "./PenlightForm/PenlightForm";
+import { MemberInfo } from "./MemberInfo/MemberInfo";
 
 export default function Home() {
   return (
     <Container>
       <Grid>
-        <Grid.Col span={{ base: 12, xs: 6 }} key="aa">
-          <MemberInfoHeader />
-          <MemberImage />
+        <Grid.Col span={{ base: 12, xs: 6 }}>
+          <MemberInfo />
         </Grid.Col>
         <Grid.Col span={{ base: 12, xs: 6 }}>
           <PenlightForm />
---
+++ view/components/Home/MemberInfo/MemberInfo.tsx
@@ -0,0 +1,11 @@
+import { MemberImage } from "../MemberImage/MemberImage";
+import { MemberInfoHeader } from "../MemberInfoHeader/MemberInfoHeader";
+
+export function MemberInfo() {
+  return (
+    <>
+      <MemberInfoHeader />
+      <MemberImage />
+    </>
+  );
+}
