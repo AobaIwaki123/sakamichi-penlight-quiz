5fc622e7a29c80dc670b1d2bd112a1b7956870d8
AobaIwaki <aobaiwaki@gmail.com>
2025-04-10 02:16:23 +0000

    update: README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# 坂道グループペンライトクイズ
+
+## 技術スタック
+
+- Nextjs
+- Mantine UI
 # 参考
 
 - [メンバーの絵文字一覧](https://w.atwiki.jp/hinatazaka46liveinfo/pages/70.html)
--- view/components/Home/MemberImage/MemberImage.tsx
+++ view/components/Home/MemberImage/MemberImage.tsx
@@ -7,7 +7,7 @@ export function MemberImage() {
     <Container size="70%">
       <Card radius="md" >
         <Card.Section>
-          <Image src="https://cdn.hinatazaka46.com/images/14/fa2/dbc520dc2a261472c591987a0a3ae/1000_1000_102400.jpg" />
+          <Image src="https://cdn.hinatazaka46.com/images/14/fa2/dbc520dc2a261472c591987a0a3ae/800_800_102400.jpg" />
         </Card.Section>
       </Card>
     </Container>
