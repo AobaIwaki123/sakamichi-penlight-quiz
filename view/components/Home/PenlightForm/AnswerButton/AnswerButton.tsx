beaba072d131f8c336f0d7c50375cb94ddf641a6
AobaIwaki <aobaiwaki@gmail.com>
2025-04-10 10:58:31 +0000

    add: 正誤判定処理

--- view/components/Home/PenlightForm/AnswerButton/AnswerButton.tsx
+++ view/components/Home/PenlightForm/AnswerButton/AnswerButton.tsx
@@ -1,8 +1,48 @@
+"use client";
+
+import { penlightColors } from '@/consts/colors';
+import { useColorStore } from '@/stores/colorStore';
 import { Button, Text } from '@mantine/core';
+import { hinatazakaPenlightColors } from '@/consts/hinatazakaPenlightColors';
 
 export function AnswerButton() {
+  const handleClick = () => {
+    const state = useColorStore.getState();
+
+    const leftIndex = state.colorMap.left?.index ?? 0;
+    const rightIndex = state.colorMap.right?.index ?? 0;
+
+
+    const leftNameJa = penlightColors[leftIndex]?.name_ja ?? '不明';
+    const rightNameJaB = penlightColors[rightIndex]?.name_ja ?? '不明';
+
+    console.log('Button clicked', leftIndex, leftNameJa, rightIndex, rightNameJaB);
+
+    // 比較対象のメンバーID
+    const targetMember = hinatazakaPenlightColors.find((m) => m.member_id === '22');
+    if (!targetMember) {
+      console.log('指定されたメンバーが見つかりません');
+      return;
+    }
+
+    const selectedSet = new Set([leftIndex.toString(), rightIndex.toString()]);
+    const memberSet = new Set([targetMember.color_id1, targetMember.color_id2]);
+
+    const isMatch =
+      selectedSet.size === memberSet.size &&
+      Array.from(selectedSet).every((val) => memberSet.has(val));
+
+    console.log('Selected Set:', selectedSet);
+    console.log('Member Set:', memberSet);
+
+    if (isMatch) {
+      console.log('一致：正解の組み合わせです');
+    } else {
+      console.log('不一致：この組み合わせは正解ではありません');
+    }
+  }
   return (
-    <Button variant="outline" radius="xl">
+    <Button variant="outline" radius="xl" onClick={handleClick}>
       <Text size="lg" fw={700}>
         回答
       </Text>
---
+++ view/consts/hinatazakaPenlightColors.ts
@@ -0,0 +1,9 @@
+export type penlightColor = {
+  member_id: string;
+  color_id1: string;
+  color_id2: string;
+}
+
+export const hinatazakaPenlightColors: penlightColor[] = [
+ { member_id: '22', color_id1: '7', color_id2: '12' },
+]
