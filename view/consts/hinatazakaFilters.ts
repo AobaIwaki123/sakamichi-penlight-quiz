bf2c45ed6aa9e8cf55339d076ab1392006d16720
AobaIwaki <aobaiwaki@gmail.com>
2025-04-12 05:31:10 +0000

    add: hinatazaka filter enum

--- view/consts/hinatazakaFilters.ts
+++ view/consts/hinatazakaFilters.ts
@@ -11,3 +11,21 @@ export const hinatazakaFilters: Filter[] = [
   { type: '5期生', defaultChecked: true },
   { type: '卒業生', defaultChecked: false },
 ]
+
+enum Generation {
+  First = "1st",
+  Second = "2nd",
+  Third = "3rd",
+  Fourth = "4th",
+  Fifth = "5th",
+  Graduated = "graduated",
+}
+
+export const GenerationMap: { [key: string]: Generation } = {
+  "1期生": Generation.First,
+  "2期生": Generation.Second,
+  "3期生": Generation.Third,
+  "4期生": Generation.Fourth,
+  "5期生": Generation.Fifth,
+  "卒業生": Generation.Graduated,
+};
