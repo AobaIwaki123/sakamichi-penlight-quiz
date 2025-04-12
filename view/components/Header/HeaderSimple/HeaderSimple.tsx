69565bef49b653a24a31806133b707d54fe5c87e
AobaIwaki <aobaiwaki@gmail.com>
2025-04-12 15:11:48 +0000

    update: header

--- view/components/Header/HeaderSimple/HeaderSimple.tsx
+++ view/components/Header/HeaderSimple/HeaderSimple.tsx
@@ -1,44 +1,18 @@
 "use client";
 
 import { Container, Group } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import { MantineLogo } from '@mantinex/mantine-logo';
-import { useState } from 'react';
-import { LightDarkToggle } from '../LightDarkToggle/LightDarkToggle';
-import classes from './HeaderSimple.module.css';
 import { FilterButton } from '../FilterButton/FilterButton';
+import { LightDarkToggle } from '../LightDarkToggle/LightDarkToggle';
 import { SakamichiLogo } from '../SakamichiLogo/SakamichiLogo';
-
-const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
-];
+import { SakamichiPenlightQuizIcon } from '../SakamichiPenlightQuizIcon/SakamichiPenlightQuizIcon';
+import classes from './HeaderSimple.module.css';
 
 export function HeaderSimple() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
-
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
-        <MantineLogo size={28} />
+        <SakamichiPenlightQuizIcon />
 
         <Group>
           <SakamichiLogo />
---
+++ view/components/Header/SakamichiPenlightQuizIcon/SakamichiPenlightQuizIcon.tsx
@@ -0,0 +1,10 @@
+import { ActionIcon, Container, Group, Text } from '@mantine/core';
+import { IconCircleDotted } from '@tabler/icons-react';
+
+export function SakamichiPenlightQuizIcon() {
+  return (
+    <ActionIcon>
+      <IconCircleDotted size={24} />
+    </ActionIcon>
+  )
+}
