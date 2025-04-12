3ffd2a76c7e80f636453757081f3f16d496517e5
AobaIwaki <aobaiwaki@gmail.com>
2025-04-12 15:20:08 +0000

    fix: member image

--- view/components/Home/MemberInfo/MemberImage/MemberImage.tsx
+++ view/components/Home/MemberInfo/MemberImage/MemberImage.tsx
@@ -3,16 +3,20 @@
 import { useSelectedMemberStore } from '@/stores/useSelectedMemberStore';
 import { Card, Container, Image, Skeleton } from '@mantine/core';
 
-
 export function MemberImage() {
   const selectedMember = useSelectedMemberStore((state) => state.selectedMember);
 
   return (
     <Container size="60%">
-      <Card radius="md" >
-        <Card.Section>
+      <Card radius="md">
+        <Card.Section style={{ height: 240, overflow: 'hidden' }}>
           {selectedMember ? (
-            <Image src={selectedMember.url} />
+            <Image
+              src={selectedMember.url}
+              height={240}
+              width="100%"
+              fit="cover" // アスペクト比を無視して埋める
+            />
           ) : (
             <Skeleton height={240} width="100%" radius="md" />
           )}
