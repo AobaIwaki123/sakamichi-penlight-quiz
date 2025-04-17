65d6bcaaab93fba93b514aa4a2e9f5879c1a9987
AobaIwaki <aobaiwaki@gmail.com>
2025-04-17 12:42:25 +0000

    fix: args

--- definitions/sources/hinatazaka/member_image_13th_single.js
+++ definitions/sources/hinatazaka/member_image_13th_single.js
@@ -1,11 +1,11 @@
 const [DATABASE_NAME, DATASET_NAME, TABLE_NAME] = [
-	"sakamichipenlightquiz_hinatazaka",
+	"sakamichipenlightquiz",
 	"hinatazaka",
 	"member_image_13th_single",
 ];
 
 declare({
 	database: DATABASE_NAME,
-	dataset: DATASET_NAME,
-	table: TABLE_NAME,
+	schema: DATASET_NAME,
+	name: TABLE_NAME,
 });
