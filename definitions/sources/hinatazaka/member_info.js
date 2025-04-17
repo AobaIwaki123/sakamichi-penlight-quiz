cb595a323ae70c3eaa50e34758a365469badb444
AobaIwaki <aobaiwaki@gmail.com>
2025-04-17 12:20:04 +0000

    add: member info source

---
+++ definitions/sources/hinatazaka/member_info.js
@@ -0,0 +1,11 @@
+const [DATABASE_NAME, DATASET_NAME, TABLE_NAME] = [
+	"sakamichipenlightquiz",
+	"hinatazaka",
+	"member_info",
+];
+
+declare({
+	database: DATABASE_NAME,
+	dataset: DATASET_NAME,
+	table: TABLE_NAME,
+});
