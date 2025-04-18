94aefda7962bd4f1eb2c71eee8eef731d716d23f
AobaIwaki <aobaiwaki@gmail.com>
2025-04-18 14:36:14 +0000

    format

--- definitions/sources/hinatazaka/member_image_13th_single.js
+++ definitions/sources/hinatazaka/member_image_13th_single.js
@@ -1,11 +1,11 @@
 const [DATABASE_NAME, DATASET_NAME, TABLE_NAME] = [
-	"sakamichipenlightquiz",
-	"hinatazaka",
-	"member_image_13th_single",
+  "sakamichipenlightquiz",
+  "hinatazaka",
+  "member_image_13th_single",
 ];
 
 declare({
-	database: DATABASE_NAME,
-	schema: DATASET_NAME,
-	name: TABLE_NAME,
+  database: DATABASE_NAME,
+  schema: DATASET_NAME,
+  name: TABLE_NAME,
 });
--- definitions/sources/hinatazaka/member_info.js
+++ definitions/sources/hinatazaka/member_info.js
@@ -1,11 +1,11 @@
 const [DATABASE_NAME, DATASET_NAME, TABLE_NAME] = [
-	"sakamichipenlightquiz",
-	"hinatazaka",
-	"member_info",
+  "sakamichipenlightquiz",
+  "hinatazaka",
+  "member_info",
 ];
 
 declare({
-	database: DATABASE_NAME,
-	schema: DATASET_NAME,
-	name: TABLE_NAME,
+  database: DATABASE_NAME,
+  schema: DATASET_NAME,
+  name: TABLE_NAME,
 });
