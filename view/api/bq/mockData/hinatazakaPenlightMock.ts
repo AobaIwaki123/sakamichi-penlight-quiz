47a28c955bbd4805c813266f1bf33e67ae2c5db9
AobaIwaki <aobaiwaki@gmail.com>
2025-09-09 17:22:29 +0900

    add: ペンライトモックデータ

---
+++ view/api/bq/mockData/hinatazakaPenlightMock.ts
@@ -0,0 +1,23 @@
+import type { PenlightColor } from '@/types/PenlightColor';
+
+/**
+ * 日向坂46のペンライト色モックデータ
+ * 開発環境でBigQueryを使用しない場合に使用される
+ */
+export const hinatazakaPenlightMock: PenlightColor[] = [
+  { id: 0, name_ja: 'パステルブルー', name_en: 'pastel_blue', color: '#09b8ff' },
+  { id: 1, name_ja: 'エメラルドグリーン', name_en: 'emerald_green', color: '#7aea9f' },
+  { id: 2, name_ja: 'グリーン', name_en: 'green', color: '#2bdd6' },
+  { id: 3, name_ja: 'パールグリーン', name_en: 'pearl_green', color: '#72ff66' },
+  { id: 4, name_ja: 'ライトグリーン', name_en: 'light_green', color: '#d3ff64' },
+  { id: 5, name_ja: 'イエロー', name_en: 'yellow', color: '#fff764' },
+  { id: 6, name_ja: 'オレンジ', name_en: 'orange', color: '#ffab09' },
+  { id: 7, name_ja: 'レッド', name_en: 'red', color: '#ff1818' },
+  { id: 8, name_ja: 'ホワイト', name_en: 'white', color: '#f5f5f5' },
+  { id: 9, name_ja: 'サクラピンク', name_en: 'sakura_pink', color: '#ff9afd' },
+  { id: 10, name_ja: 'ピンク', name_en: 'pink', color: '#ff18f6' },
+  { id: 11, name_ja: 'パッションピンク', name_en: 'passion_pink', color: '#ff0988' },
+  { id: 12, name_ja: 'バイオレット', name_en: 'violet', color: '#c029df' },
+  { id: 13, name_ja: 'パープル', name_en: 'purple', color: '#9462d2' },
+  { id: 14, name_ja: 'ブルー', name_en: 'blue', color: '#1d72fe' },
+];
